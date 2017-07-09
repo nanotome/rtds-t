96ba488619385e3800b66034341c399b9b78a307
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-09 09:22:01 +0200

    feat(player): use right analog stick to turn Player
    - Configure XBOX 360 controller settings for the game. Check images
    attached to this commit message.

--- RTDS-T/Assets/Scripts/Player/Player.cs
+++ RTDS-T/Assets/Scripts/Player/Player.cs
@@ -1,5 +1,6 @@
 ﻿// The Player script receives input for the Player.
 
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof (PlayerController))]
@@ -10,6 +11,7 @@ public class Player : MonoBehaviour {
     Camera viewCamera;
 
     float moveSpeed = 5f;
+    float lookSpeed = 20f; // turning speed for game controller
 
 	void Start () {
         playerController = GetComponent<PlayerController>();
@@ -21,24 +23,35 @@ public class Player : MonoBehaviour {
         Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         playerController.Move(moveInput.normalized * moveSpeed);
 
-        // Turn Player to face the direction of the cursor
-        // The screen is a 2d world but the game is in a 3d world so we need raycasting to get the
-        // accurate position of the mouse cursor in the game world.
-
-        // 1. Cast a ray from the camera through the mouse cursor's position
-        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
-        // 2. draw a plane that's perpendicular to the y-axis and passes through a point at the gun height.
-        // this would be a plane just above the ground and slicing the player at gun height.
-        Plane groundPlane = new Plane(Vector3.up, Vector3.up * 1); // 1 here represents the height of the gun
-        float rayDistance;
-
-        // 3. if the ray drawn earlier intersects the plane
-        if (groundPlane.Raycast(ray, out rayDistance))
+        // Turn Player
+        if (Input.GetJoystickNames().Contains("Controller (XBOX 360 For Windows)"))
         {
-            // 4. get the point where the ray intersects the plane
-            Vector3 point = ray.GetPoint(rayDistance);
-            // 5. turn the Player to face that point
-            playerController.LookAt(point);
+            // If an XBOX 360 pad is attached, turn the Player according to
+            // the movement of the right analog stick.
+            Vector3 lookPoint = new Vector3(Input.GetAxisRaw("XBOXRightAnalogH"), 0, Input.GetAxisRaw("XBOXRightAnalogV"));
+            Vector3 newPoint = lookPoint.normalized * lookSpeed;
+            playerController.LookAt(newPoint);
+        } else
+        {
+            // Turn Player to face the direction of the cursor
+            // The screen is a 2d world but the game is in a 3d world so we need raycasting to get the
+            // accurate position of the mouse cursor in the game world.
+
+            // 1. Cast a ray from the camera through the mouse cursor's position
+            Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+            // 2. draw a plane that's perpendicular to the y-axis and passes through a point at the gun height.
+            // this would be a plane just above the ground and slicing the player at gun height.
+            Plane groundPlane = new Plane(Vector3.up, Vector3.up * 1); // 1 here represents the height of the gun
+            float rayDistance;
+
+            // 3. if the ray drawn earlier intersects the plane
+            if (groundPlane.Raycast(ray, out rayDistance))
+            {
+                // 4. get the point where the ray intersects the plane
+                Vector3 point = ray.GetPoint(rayDistance);
+                // 5. turn the Player to face that point
+                playerController.LookAt(point);
+            }
         }
 	}
 }
