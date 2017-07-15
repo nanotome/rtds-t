f802ca032717cd264e553514355e923e0817343d
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-15 08:56:32 +0200

    feat(items): define IItemCase interface
    IItemCase are objects that release items when they 'die'. For example,
    an Enemy that drops health or ammo would need to implement IItemCase.

---
+++ RTDS-T/Assets/Scripts/Collectibles/IItemCase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// IItemCase are objects that release items when they 'die'. For example, an
+// Enemy that drops health or ammo would need to implement IItemCase
+public interface IItemCase
+{
+    // Explosion behaviour is common to both enemies and destructible obstacles so
+    // it will be placed here.
+    // This method instantiates the particles
+    void Explode(ParticleSystem deathEffect, Vector3 hitPoint, Vector3 hitDirection);
+    void SpawnItems(List<Pack> items);
+}
