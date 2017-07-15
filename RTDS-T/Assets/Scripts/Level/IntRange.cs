70fa9380cc1999a2f1f03481864a78207762760c
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-16 00:43:34 +0200

    feat(levels): define IntRange for integer ranges
    - Move BoardManger to 'Level' folder in 'Scripts'.

---
+++ RTDS-T/Assets/Scripts/Level/IntRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public struct IntRange {
+
+    public int m_Min;
+    public int m_Max;
+
+    public IntRange(int min, int max)
+    {
+        m_Min = min;
+        m_Max = max;
+    }
+
+    public int RandomInt
+    {
+        get
+        {
+            return Random.Range(m_Min, m_Max);
+        }
+    }
+}
