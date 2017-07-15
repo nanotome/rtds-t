c834378c9aaabc5b742d1d4a04ad1638028eaa61
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-15 23:43:30 +0200

    feat(levels): define GameManager object
    The GameManager is responsible for creating the game board for each
    level and populating it with entities. The BoardHolder child of the
    GameManager is the container for the generated board.
    
    - Create GameManager prefab.
    - Create GameManager script
    - Make GameManager script a singleton.
    - Attach GameManager script to GameManager prefab.
    - Create BoardHolder child object of GameManager prefab.
    - Create Loader script to load GameManager and attach to Main Camera.

---
+++ RTDS-T/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        // Ensure the instance is of the type GameManager
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        // Persist the GameManager instance across scenes
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ RTDS-T/Assets/Scripts/Loader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    public GameObject gameManager;
+
+    private void Awake()
+    {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+    }
+}
