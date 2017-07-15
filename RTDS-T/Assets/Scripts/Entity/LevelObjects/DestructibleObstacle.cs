e1bb24ceb4e621363faf7c68c9c33759b523d669
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-15 23:06:47 +0200

    feat(levels): define destructible obstacle prefab
    A destrubtible obstacles can be broken apart by shooting it multiple
    times. It has a health property that determines how many shots are
    needed to destroy it.
    - Place DestructibleObstacle on the 'Entity' layer.

---
+++ RTDS-T/Assets/Scripts/Entity/LevelObjects/DestructibleObstacle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObstacle : LivingEntity, IItemCase
+{
+    public ParticleSystem deathEffect;
+    // List of items that may be spawned when the obstacle is destroyed dies
+    public List<Pack> deathItems;
+
+    System.Random prng = new System.Random();
+
+    public void Explode(ParticleSystem deathEffect, Vector3 hitPoint)
+    {
+        Destroy(Instantiate(deathEffect.gameObject, hitPoint, transform.rotation));
+    }
+
+    public void SpawnItems(List<Pack> items)
+    {
+        // pick a random item from the list of items and spawn it
+        Pack itemToSpawn = deathItems[prng.Next(items.Count)];
+        Instantiate(itemToSpawn, transform.position, transform.rotation);
+    }
+
+    public override void Die()
+    {
+        Explode(deathEffect, transform.position);
+        SpawnItems(deathItems);
+        base.Die();
+    }
+
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+    }
+}
