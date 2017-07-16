67f2314c44cf3c76be9d7aebb0d0ee6e0c605878
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-16 08:24:59 +0200

    feat(levels): define TileInfo struct
    A TileInfo struct contains all the information about a position in the
    map's grid. The information includes:
    - The tile's position on the map's grid
    - whether the tile is a room, corridor or wall
    - the id of the room or corridor
    - the type of prefab that will be placed in the TileInfo's position.
    This enables quick filtering of the map.
    - A list of prefabs that can be spawned in the TileInfo's position based
    on the type of prefab.
    
    The TileInfo struct is defined in the BoardManager because it requires
    references to the TilePosition and PrefabType enums.

--- RTDS-T/Assets/Scripts/Level/BoardManager.cs
+++ RTDS-T/Assets/Scripts/Level/BoardManager.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Random = System.Random;
+using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
+
+    public enum TilePosition { Wall, Room, Corridor };
+    public enum PrefabType
+    {
+        Wall, Obstacles, Enemy, Player
+    };
 
     public int columns = 50;
     public int rows = 50;
@@ -22,4 +31,42 @@ public class BoardManager : MonoBehaviour {
     void Start () {
         boardHolder = transform.Find("BoardHolder").transform;
 	}
+
+    [Serializable]
+    public struct TileInfo
+    {
+        // The position of the tile on the map's grid.
+        public Coord pos;
+        // The TilePosition determines if the tile is a wall or if it's in
+        // a room or corridor.
+        public TilePosition position;
+        // ID of the room or corridor
+        public string id;
+
+        // The type of initial prefab placed on this tile (on the Floor tile).
+        public PrefabType prefabType;
+        // List of prefabs this tile can spawn.
+        public List<GameObject> prefabs;
+
+        Random prng;
+
+        public TileInfo(Coord mapPos, int xPos, int yPos, TilePosition tilePos, string posId)
+        {
+            pos = mapPos;
+            position = tilePos;
+            id = posId;
+
+            prefabType = PrefabType.Wall;
+            prefabs = new List<GameObject>();
+
+            prng = new Random();
+        }
+
+        public void SpawnPrefab()
+        {
+            // pick a random prefab from the list of prefabs and spawn it
+            GameObject itemToSpawn = prefabs[prng.Next(prefabs.Count)];
+            Instantiate(itemToSpawn, new Vector3(pos.x, 0, pos.y), Quaternion.identity);
+        }
+    }
 }
