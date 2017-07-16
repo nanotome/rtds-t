90835f12c7e9a49ec25fafd4380dfa3b20de38b2
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-16 07:24:19 +0200

    feat(levels): define Coord for level map coordinates

---
+++ RTDS-T/Assets/Scripts/Level/Coord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+[Serializable]
+public struct Coord
+{
+
+    public int x;
+    public int y;
+
+    public Coord(int xPos, int yPos)
+    {
+        x = xPos;
+        y = yPos;
+    }
+
+    public static bool operator ==(Coord c1, Coord c2)
+    {
+        return c1.x == c2.x && c1.y == c2.y;
+    }
+
+    public static bool operator !=(Coord c1, Coord c2)
+    {
+        return !(c1 == c2);
+    }
+}
