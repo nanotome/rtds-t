41aac2f9b20311a5e72672c1167fc2cda15bdcf7
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-07-21 07:31:15 +0200

    feat(ui): main menu
    - Import custom font
    - Create menu scene
    - Show main menu on app start
    - Show 'Play' button in menu
    - Start game when 'Play' button is clicked

---
+++ RTDS-T/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
